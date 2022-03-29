c371d59dbbe38da6d16eb910748a0e15b381a293
Arnaud Courtès <arnaud@courtes.fr>
2022-03-29 02:40:16 +0200

    Added a FIXME

--- src/main/java/fr/arolla/ScalarComputation.java
+++ src/main/java/fr/arolla/ScalarComputation.java
@@ -2,6 +2,7 @@ package fr.arolla;
 
 public class ScalarComputation {
 
+    // FIXME Try to use the new Vector API here by following the documentation (see README file)
     void compute(float[] a, float[] b, float[] c) {
         for (int i = 0; i < a.length; i++) {
             c[i] = (a[i] * a[i] + b[i] * b[i]) * -1.0f;
